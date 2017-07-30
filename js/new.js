f9be2df94790c0ca3a11e84a49f8c382519de691
Julian Wolf <julian.wolf@student.tugraz.at>
2017-07-30 19:20:46 +0200

    Added REST api and first iteration of input

---
+++ js/new.js
@@ -0,0 +1,31 @@
+function newPart(event) {
+    obj = {};
+    $('#newPart input').each(function (index) {
+        obj[($(this).attr('name'))] = $(this).val();
+    });
+
+    $('#newPart select').each(function (index) {
+        obj[($(this).attr('name'))] = $(this).val();
+    });
+
+    $('#newPart textarea').each(function (index) {
+        obj[($(this).attr('name'))] = $(this).val();
+    });
+
+    //$('#newPart')[0].reset();
+
+    $.ajax({
+        method: 'post',
+        url: 'rest/api.php/parts',
+        data: JSON.stringify(obj)
+    }).done( function (event) {
+        alert('success');
+        console.log(event);
+    }).fail (function ( event ) {
+        alert("error happened");
+        console.log(event);
+    });
+
+    event.preventDefault();
+    return false;
+}
